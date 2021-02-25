908bd0e8201fb3bd58b2cd3cfd1be723008c891f
Eds <steadyede@live.co.uk>
2021-02-25 15:22:21 +0000

    Hyperlinks for bookmark title

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,6 +2,14 @@ require 'pg'
 
 class Bookmark
 
+  attr_reader :id, :title, :url
+
+  def initialize(id:, title:, url:)
+    @id = id
+    @title = title
+    @url = url
+  end
+
   def self.create(url:, title:)
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'bookmark_manager_test')
@@ -18,7 +26,10 @@ class Bookmark
       connection = PG.connect :dbname => 'bookmark_manager'
     end
     result = connection.exec "SELECT * FROM bookmarks"
-    result.map { |row| row['title'] }
+    result.map do |bookmark|
+      Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
+    end 
+    
   end
 
 end
